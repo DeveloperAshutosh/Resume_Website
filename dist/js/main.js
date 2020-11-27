bcbe8afb9ecbf6d94c0476d93a48551806f488d1
ashutosh <akausha1@ualberta.ca>
2020-11-26 21:28:52 -0700

    added styling to hamburger btn

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,4 +1,31 @@
+alert("hello world");
 const menuBtn = document.querySelector('.btn');
 const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
+const navItems = document.querySelectorAll('.nav-item');
+let showMenu = false;
+menuBtn.addEventListener('click',(e)=>{
+    if(!showMenu){
+        menuBtn.classList.add('close');
+        menu.classList.add('show');
+        menuBranding.classList.add('show');
+        menuNav.classList.add('show');
+        navItems.forEach(item => item.classList.add('show'));
+        //set menu state
+        showMenu = true;
+
+    }
+    else{
+        menuBtn.classList.remove('close');
+        menu.classList.remove('show');
+        menuBranding.classList.remove('show');
+        menuNav.classList.remove('show');
+        navItems.forEach(item => item.classList.remove('show'));
+        
+        //set menu state
+        showMenu = false;
+        
+    }
+
+});

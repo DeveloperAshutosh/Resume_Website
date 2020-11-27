ffcecad0f6f1918117eba97bfacea8c04d68a83d
ashutosh <akausha1@ualberta.ca>
2020-11-26 20:25:50 -0700

    added hamnurger btn

---
+++ dist/js/main.js
@@ -0,0 +1,4 @@
+const menuBtn = document.querySelector('.btn');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBranding = document.querySelector('.menu-branding');
